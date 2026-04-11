b73c3bb6a57c03ef3c1d6c7472c392154900c46e
aryanX21 <2025.aryanprasad@gmail.com>
2026-04-11 22:55:40 +0530

    Update electricity bill calculator using slab-based logic

--- Conditionals/Problems/hard_01.cpp
+++ Conditionals/Problems/hard_01.cpp
@@ -1,21 +1,20 @@
 /*
-    Program: Electricity Bill Calculator (Simple Version)
+    Program: Electricity Bill Calculator (Slab-Based Version)
     Description:
     This program takes the number of electricity units consumed
-    as input and calculates the total bill based on different rates.
+    as input and calculates the total bill using a slab-based system.
 
-    Different rates are applied depending on the range of units.
+    In a slab system, different rates are applied to different portions
+    of the total units, not the entire unit at once.
 
     Problem Statement:
-    Write a program to calculate electricity bill using the following rates:
-    - 0–100 units  → ₹5 per unit
-    - 101–150 units → ₹7 per unit
-    - 151–200 units → ₹8.5 per unit
-    - Above 200 units → ₹10 per unit
+    Write a program to calculate electricity bill using the following slabs:
+    - First 100 units     → ₹5 per unit
+    - Next 50 units       → ₹7 per unit (101–150)
+    - Next 50 units       → ₹8.5 per unit (151–200)
+    - Above 200 units     → ₹10 per unit
 
-    Note:
-    This program uses a simple range-based approach (not slab system).
-    Will update it later.
+    The bill should be calculated by adding charges from each slab step by step.
 */
 
 #include<iostream>
@@ -23,7 +22,7 @@ using namespace std;
 
 int main(){
 
-    double unit,bill;
+    double unit,bill,remunit;
 
     cout<<"Enter your unit: ";
     cin>>unit;
@@ -32,13 +31,16 @@ int main(){
         bill = unit*5;
     }
     else if(unit <= 150){
-        bill = unit*7;
+        remunit = unit - 100;
+        bill = (remunit*7 + 5*100);
     }
     else if(unit <= 200){
-        bill = unit*8.5;
-    }
+        remunit = unit - 150;
+        bill = (remunit*8.5 + 50*7 + 100*5);
+    }    
     else{
-        bill = unit * 10;
+        remunit = unit - 200;
+        bill = (remunit*10 + 100*5 + 50*7 + 50*8.5);
     }
 
     cout<<"Bill: "<<bill<<" Rs."<<endl;
