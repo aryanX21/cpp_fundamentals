14b12c2e2a92a2704658f408fdb8097a7cccd9a9
aryanX21 <2025.aryanprasad@gmail.com>
2026-04-20 22:34:01 +0530

    Add basic function program demonstrating function syntax

--- Functions/Inroduction.cpp
+++ Functions/Inroduction.cpp
@@ -1,25 +1,27 @@
+/*
+    Program: Basic Function in C++
+    Description:
+    A function is a block of code that performs a specific task.
+    It helps in organizing code and avoiding repetition.
+
+    Syntax:
+    return_type function_name(){
+        // code
+    }
+*/
+
 #include<iostream>
 using namespace std;
 
-int sum(int a, int b){  // int a, int b are parameters
-    int s = a + b;
-    return s;
+// Function definition
+void greet(){
+    cout<<"Hello, Aryan"<<endl;
 }
 
-int min(int a, int b){
-    if(a>b){
-       return b;
-    }
-    else{
-     return a;
-    }
-}
 int main(){
-    cout<<sum(12,22);
-    cout<<endl;
 
-    cout<<min(2,22)<<" is min";  //  2,22 are arguments or actual value of functions
+    // Function call
+    greet();
 
-return 0;
-
+    return 0;
 }
