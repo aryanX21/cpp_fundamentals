227df00120b77be56c594da9f604544c93cbcbcb
aryanX21 <2025.aryanprasad@gmail.com>
2026-04-30 21:51:14 +0530

    Updated program of reverse string using length and index swapping

--- Strings/problems/easy_02.cpp
+++ Strings/problems/easy_02.cpp
@@ -3,7 +3,8 @@
 
     Description:
     This program reverses a string manually
-    using two-pointer technique and swapping.
+    by calculating its length and swapping
+    characters from both ends.
 */
 
 #include<iostream>
@@ -11,26 +12,26 @@
 using namespace std;
 
 int main(){
-
+    
     string str;
-
     cout<<"Enter string: ";
     getline(cin,str);
 
-    int start = 0;
-    int end = str.length()-1;
+    int l = 0;
+    for(int i = 0; str[i] != '\0'; i++){
+        l++;
+    }
 
-    while(start < end){
+    //cout<<"Length of string:  "<<length<<endl;
 
-        char temp = str[start];
-        str[start] = str[end];
-        str[end] = temp;
-
-        start++;
-        end--;
+    for(int i = 0; i < l/2; i++){
+        char temp;
+        temp = str[i];
+        str[i] = str[l - (i + 1)];
+        str[l - (i + 1)] = temp;
     }
 
     cout<<"Reversed string: "<<str<<endl;
 
-    return 0;
+    return 0; 
 }
