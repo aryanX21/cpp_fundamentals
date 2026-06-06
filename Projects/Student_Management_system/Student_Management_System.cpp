5fa27e19247ab18d54811c0dce7598c131f786b5
aryanX21 <2025.aryanprasad@gmail.com>
2026-06-06 23:53:50 +0530

    Build student management system using OOP and file handling

--- Projects/Student_Management_system/Student_Management_System.cpp
+++ Projects/Student_Management_system/Student_Management_System.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include<fstream>
 using namespace std;
 
 class Student{
@@ -13,7 +14,7 @@ class Student{
 
         cin.ignore();
 
-        cout<<"Enter name: ";
+        cout<<"\nEnter name: ";
         getline(cin,name);
 
         cout<<"Enter id: ";
@@ -57,8 +58,10 @@ int total_students = 0;
 void add_student()
 {
     int num;
-
+    
+    cout<<"\nEnter number of students: ";
     cin>>num;
+    
 
     for(int i = 0; i < num; i++)
     {
@@ -72,17 +75,157 @@ void display_data(){
 
     cout<<"\n\n --- Students Data ---  \n\n";
 
+    if(total_students == 0){
+        cout<<"You have not added any Student's data"<<endl;
+        return;
+    }
+
     for(int i = 0; i < total_students; i++){
+        cout<<"\n-------------------\n";
         students[i].display();
+        cout<<"-------------------\n";
     }
 }
 
+void search(){
+
+    int roll_no;
+    cout<<"\nEnter the roll number of the Student: ";
+    cin>>roll_no;
+
+    bool found = false;
+
+    for(int i = 0; i < total_students; i++){
+        if(students[i].roll ==  roll_no){
+            cout<<"\nStudent Found!\n"<<endl;
+            cout<<"\n-------------------\n";
+            students[i].display();
+            cout<<"-------------------\n";
+            found = true;
+            break;
+        }
+    }
+
+    if(!found){
+       cout<<"\nStudent not found"<<endl;
+    }
+    
+}
+
+void update(){
+
+    int roll_no;
+
+    cout<<"Enter the Student's roll number you want to update: ";
+    cin>>roll_no;
+
+    bool found = false;
+
+    for(int i = 0; i < total_students; i++){
+        if(students[i].roll ==  roll_no){
+            cout<<"\nUpdate the details"<<endl;
+            students[i].get_data();
+            found = true;
+            break;
+        }
+    }
+
+    if(!found){
+       cout<<"\nStudent not found"<<endl;
+    }
+}
+
+void del(){
+
+    int roll_no;
+    bool found = false;
+
+    cout<<"Enter the Student's roll number you want to delete: ";
+    cin>>roll_no;
+
+    for(int i = 0; i < total_students; i++){
+
+        if(students[i].roll == roll_no){
+
+            for(int j = i; j < total_students - 1; j++){
+
+                students[j] = students[j + 1];
+            }
+
+            total_students--;
+
+            cout<<"\nStudent deleted successfully\n";
+
+            found = true;
+            break;
+        }
+    }
+
+    if(!found){
+
+        cout<<"\nStudent not found\n";
+    }
+}
+
+void save_data(){
+
+    ofstream fout("students.txt");
+
+    for(int i = 0; i < total_students; i++){
+
+        fout << students[i].name << endl;
+        fout << students[i].id << endl;
+        fout << students[i].roll << endl;
+        fout << students[i].age << endl;
+        fout << students[i].Course << endl;
+        fout << students[i].Branch << endl;
+        fout << students[i].Specialization << endl;
+    }
+
+    fout.close();
+
+    cout<<"\nData saved successfully\n";
+}
+
+void load_data(){
+
+    ifstream fin("students.txt");
+
+    if(!fin){
+
+        cout<<"\nNo saved data found\n";
+        return;
+    }
+
+    total_students = 0;
+
+    while(getline(fin, students[total_students].name)){
+
+        getline(fin, students[total_students].id);
+
+        fin >> students[total_students].roll;
+        fin >> students[total_students].age;
+
+        fin.ignore();
+
+        getline(fin, students[total_students].Course);
+        getline(fin, students[total_students].Branch);
+        getline(fin, students[total_students].Specialization);
+
+        total_students++;
+    }
+
+    fin.close();
+
+    cout<<"\nData loaded successfully\n";
+}
 
 int main(){
 
     cout<<"\n\n ----  Welcome to Student Management System  ----\n\n";
 
-    cout<<"Choose from the below - \n\n";
+    while(true){
+    cout<<"\n\nChoose from the below - \n\n";
 
     cout<<"1) Add Student\n";
     cout<<"2) Display Students\n";
@@ -91,6 +234,7 @@ int main(){
     cout<<"5) Delete Student\n";
     cout<<"6) Save Data to File\n";
     cout<<"7) Load Data from File\n";
+    cout<<"8) Exit\n";
 
         
     int n;
@@ -108,22 +252,31 @@ int main(){
         break;
 
         case 3: 
+            search();
         break;
 
         case 4: 
+            update();           
         break;
 
         case 5: 
+            del();
         break;
 
         case 6: 
+            save_data();
         break;
 
         case 7: 
+            load_data();
         break;
 
+        case 8:
+           cout<<"\nThank You For Using Student Management System\n";
+        return 0;
+
         default:
-            cout<<"Invalid Option\n"; 
+            cout<<"\nInvalid Option\n"; 
         }
-  
-}
+    }
+}   
